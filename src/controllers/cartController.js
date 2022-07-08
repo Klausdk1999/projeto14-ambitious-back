b4798f70dd014a01a5bd85257ef49b472ca550a6
Klaus Dieter Kupper <klausdk@pop-os.localdomain>
2022-07-08 20:50:51 -0300

    fixed cart route

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,24 +1,20 @@
 import { db, objectId } from '../dbStrategy/mongo.js';
 import joi from 'joi';
 
-//export async function getCart(req, res) {
- // const session = res.locals.session;
+export async function getCart(req, res) {
+ const session = res.locals.session;
 
-  // const posts = await db
-  //   .collection('transactions')
-  //   .find({ userId: new objectId(session.userId) })
-  //   .toArray();
-  // res.send(posts);
-//}
+  const posts = await db
+    .collection('transactions')
+    .find({ userId: new objectId(session.userId) })
+    .toArray();
+  res.send(posts);
+}
 
 export async function insertCart(req, res) {
   const id = req.body;
   const { authorization } = req.headers;
   const token = authorization?.replace('Bearer ', '');
-
-  console.log("insert cart");
-
-  console.log(token);
 
   const session = await db.collection('sessoes').findOne({ token });
   console.log(session);
@@ -27,23 +23,21 @@ export async function insertCart(req, res) {
   }
 
   try {
-
-		const user = await db.collection("usuarios").findOne({ _id: new ObjectId(session.userId) })
+		const user = await db.collection("usuarios").findOne({ _id: session.userId })
+    
 		if (!user) {
 			res.sendStatus(404)
 			return;
 		}
     
     const carrinho=user.cart;
-    console.log("cria carrinho"+carrinho);
     carrinho.push(id);
-    console.log("push"+carrinho);
-    console.log("id"+user.id)
+    
 		await db.collection("usuarios").updateOne({ 
 			_id: user._id 
 		}, { $set: { "cart": carrinho } });
 				
-		res.send("Inserido item no carrinho").status(200);
+		res.send("Item inserido  no carrinho").status(200);
 		
 	  } catch (error) {
 	  res.status(500).send(error)
